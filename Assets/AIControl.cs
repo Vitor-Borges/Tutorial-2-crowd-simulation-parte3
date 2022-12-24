31d6e0543725d8594d01506d1698267e89b60476
Vitor-Borges <62852982+Vitor-Borges@users.noreply.github.com>
2022-12-24 03:25:00 +0000

    fleeing
    Etapa completa.

---
+++ Assets/AIControl.cs
@@ -0,0 +1,71 @@
+﻿using System.Dynamic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UIElements;
+
+public class AIControl : MonoBehaviour {
+
+    GameObject[] goalLocations;
+    UnityEngine.AI.NavMeshAgent agent;
+    Animator anim;
+    float speedMult;
+    float detectionRadius = 5;
+    float fleeRadius = 10;
+
+
+    void ResetAgent()
+    {
+        speedMult = Random.Range(0.1f, 1.5f);
+        agent.speed = 2 * speedMult;
+        agent.angularSpeed = 120;
+        anim.SetFloat("speedMult", speedMult);
+        anim.SetTrigger("isWalking");
+        agent.ResetPath();
+    }
+
+    public void DetectNewObstacle(Vector3 position)
+    {
+        if (Vector3.Distance(position, this.transform.position) < detectionRadius)
+        {
+            Vector3 fleeDirection = (this.transform.position - position).normalized;
+            Vector3 newgoal = this.transform.position + fleeDirection * fleeRadius;
+            
+            NavMeshPath path = new NavMeshPath();
+            agent.CalculatePath(newgoal, path);
+
+            if (path.status != NavMeshPathStatus.PathInvalid)
+            {
+                agent.SetDestination(path.corners[path.corners.Length - 1]); 
+                anim.SetTrigger("isRunning");
+                agent.speed = 10;
+                agent.angularSpeed = 500;
+            }
+        }
+    }
+
+
+
+
+    void Start() {
+        goalLocations = GameObject.FindGameObjectsWithTag("goal");
+        agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.SetDestination(goalLocations[Random.Range(0, goalLocations.Length)].transform.position);
+        anim = this.GetComponent<Animator>();
+        anim.SetFloat("wOffset", Random.Range(0.0f, 1.0f));
+        ResetAgent();
+        //anim.SetTrigger("isWalking");
+        //float sm = Random.Range(0.1f, 1.5f);
+        //anim.SetFloat("speedMult", sm);
+        //agent.speed *= sm;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (agent.remainingDistance < 1) {
+
+            ResetAgent();
+            agent.SetDestination(goalLocations[Random.Range(0, goalLocations.Length)].transform.position);
+        }
+    }
+}
